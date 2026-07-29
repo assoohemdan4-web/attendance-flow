b039161cdf43e423476d1625c2b62bb36d6f15ea
assoohemdan4-web <assoohemdan4@gmail.com>
2026-07-30 02:34:43 +0300

    Alert number of rows from the Excel sheet
    Added functionality to alert the number of rows in the sheet.

--- admin.js
+++ admin.js
@@ -13,7 +13,13 @@ input.addEventListener("change", function (e) {
 
         const workbook = XLSX.read(data, { type: "array" });
 
-        alert("Sheets:\n\n" + workbook.SheetNames.join("\n"));
+        const sheet = workbook.Sheets["داتا"];
+
+        const rows = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+        alert("عدد الصفوف = " + rows.length);
+
+        console.log(rows);
 
     };
 
